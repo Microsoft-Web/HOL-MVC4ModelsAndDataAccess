8f0bd3773f1a12b0bf5ad4d52c6c6ab9cb834488
Jorge Rowies <jorge.rowies@southworks.net>
2012-03-23 17:15:11 -0300

    adding exercises #2 and #3

---
+++ source/Ex03-QueryingTheDatabaseWithParametersCodeFirst/Begin/MvcMusicStore/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+    }
+}
